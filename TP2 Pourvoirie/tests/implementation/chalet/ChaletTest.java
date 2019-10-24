a517781a955ec59abe4c8ecd8d577fd8ffdd4786
charlest360 <47825792+charlest360@users.noreply.github.com>
2019-10-24 09:13:21 -0400

    Base du TP2

---
+++ TP2 Pourvoirie/tests/implementation/chalet/ChaletTest.java
@@ -0,0 +1,21 @@
+package implementation.chalet;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ChaletTest {
+	
+	@Test 
+	public void GIVEN_A_4_PERSONS_CHALET_WHEN_CALLING_GETPRICE_THEN_THE_RIGHT_PRICE_IS_RETURNED() {
+
+		//Arrange
+		
+		
+		//Act
+		
+		
+		
+		//Assert
+		assertEquals(0,0);		
+	}
+}
